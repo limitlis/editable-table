1b443b5b982f13eb6163e37cda57cc20fcb7b0f5
Cesar Vargas <limitlis@gmail.com>
2016-05-05 15:47:56 -0700

    Allow for select with options

--- mindmup-editabletable.js
+++ mindmup-editabletable.js
@@ -2,29 +2,95 @@
 $.fn.editableTableWidget = function (options) {
 	'use strict';
 	return $(this).each(function () {
+		function bindEvents () {
+			editor.blur(function () {
+					setActiveText();
+					editor.hide();
+				}).keydown(function (e) {
+					if (e.which === ENTER) {
+						setActiveText();
+						editor.hide();
+						active.focus();
+						e.preventDefault();
+						e.stopPropagation();
+					} else if (e.which === ESC) {
+						editor.val(active.text());
+						e.preventDefault();
+						e.stopPropagation();
+						editor.hide();
+						active.focus();
+					} else if (e.which === TAB) {
+						active.focus();
+					} else if (this.selectionEnd - this.selectionStart === this.value.length) {
+						var possibleMove = movement(active, e.which);
+						if (possibleMove.length > 0) {
+							possibleMove.focus();
+							e.preventDefault();
+							e.stopPropagation();
+						}
+					}
+				})
+				.on('input paste', function () {
+					var evt = $.Event('validate');
+					active.trigger(evt, editor.val());
+					if (evt.result === false) {
+						editor.addClass('error');
+					} else {
+						editor.removeClass('error');
+					}
+				});
+			}
+
 		var buildDefaultOptions = function () {
 				var opts = $.extend({}, $.fn.editableTableWidget.defaultOptions);
-				opts.editor = opts.editor.clone();
+				opts.editorText = opts.editorText.clone();
+				opts.editorSelect = opts.editorSelect.clone();
 				return opts;
 			},
 			activeOptions = $.extend(buildDefaultOptions(), options),
 			ARROW_LEFT = 37, ARROW_UP = 38, ARROW_RIGHT = 39, ARROW_DOWN = 40, ENTER = 13, ESC = 27, TAB = 9,
 			element = $(this),
-			editor = activeOptions.editor.css('position', 'absolute').hide().appendTo(element.parent()),
+			editor,
+			editorText = activeOptions.editorText.css('position', 'absolute').hide().appendTo(element.parent()),
+			editorSelect = activeOptions.editorSelect.css('position', 'absolute').hide().appendTo(element.parent()),
 			active,
 			showEditor = function (select) {
 				active = element.find('td:focus:not(' + activeOptions.skipClass + ')');
 				if (active.length) {
-					editor.val(active.text())
-						.removeClass('error')
-						.show()
-						.offset(active.offset())
-						.css(active.css(activeOptions.cloneProperties))
-						.width(active.width())
-						.height(active.height())
-						.focus();
-					if (select) {
-						editor.select();
+					if (!active.data('type-options')) {
+						editor = editorText.val(active.text())
+							.removeClass('error')
+							.show()
+							.offset(active.offset())
+							.css(active.css(activeOptions.cloneProperties))
+							.width(active.width())
+							.height(active.height())
+							.focus();
+						bindEvents();
+
+					} else if (active.data('type-options')) {
+						editorSelect.children('option').remove();
+
+						// Create options based on data-select-options
+						var tempOptions = active.data('type-options'),
+							selected = false;
+						if (tempOptions.length) {
+							for(var i = 0; i < tempOptions.length; i++) {
+								if (active.text() === tempOptions[i]) {
+									selected = true;
+								}
+								editorSelect.append($('<option value="' + tempOptions[i] + '" ' + (selected ? 'selected' : '') +'>' + tempOptions[i] + '</option>'));
+							}
+							editor = editorSelect.val(active.text())
+								.removeClass('error')
+								.show()
+								.offset(active.offset())
+								.css(active.css(activeOptions.cloneProperties))
+								.width(active.width())
+								.height(active.outerHeight())
+								.focus();
+							bindEvents();
+						}
 					}
 				} else {
 					// Propogate click down to first child of td
@@ -58,42 +124,6 @@ $.fn.editableTableWidget = function (options) {
 				}
 				return [];
 			};
-		editor.blur(function () {
-			setActiveText();
-			editor.hide();
-		}).keydown(function (e) {
-			if (e.which === ENTER) {
-				setActiveText();
-				editor.hide();
-				active.focus();
-				e.preventDefault();
-				e.stopPropagation();
-			} else if (e.which === ESC) {
-				editor.val(active.text());
-				e.preventDefault();
-				e.stopPropagation();
-				editor.hide();
-				active.focus();
-			} else if (e.which === TAB) {
-				active.focus();
-			} else if (this.selectionEnd - this.selectionStart === this.value.length) {
-				var possibleMove = movement(active, e.which);
-				if (possibleMove.length > 0) {
-					possibleMove.focus();
-					e.preventDefault();
-					e.stopPropagation();
-				}
-			}
-		})
-		.on('input paste', function () {
-			var evt = $.Event('validate');
-			active.trigger(evt, editor.val());
-			if (evt.result === false) {
-				editor.addClass('error');
-			} else {
-				editor.removeClass('error');
-			}
-		});
 		element.on('click keypress dblclick', showEditor)
 		.css('cursor', 'pointer')
 		.keydown(function (e) {
@@ -116,7 +146,6 @@ $.fn.editableTableWidget = function (options) {
 		});
 
 		element.find('td').prop('tabindex', 1);
-
 		$(window).on('resize', function () {
 			if (editor.is(':visible')) {
 				editor.offset(active.offset())
@@ -132,6 +161,7 @@ $.fn.editableTableWidget.defaultOptions = {
 					'text-align', 'font', 'font-size', 'font-family', 'font-weight',
 					'border', 'border-top', 'border-bottom', 'border-left', 'border-right'],
 	skipClass: '.noedit',
-	editor: $('<input>')
+	editorText: $('<input>'),
+	editorSelect: $('<select>')
 };
 
